0abcd891c1b0476511bc87005442e03fb26aec1e
Kenneth Yang <kjy5@uw.edu>
2024-01-04 23:54:15 -0800

    Setup MUI

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import { CssBaseline } from '@mui/material';
+
 const root = document.getElementById('root');
 if (root) {
   ReactDOM.createRoot(root).render(
     <React.StrictMode>
+      <CssBaseline />
       <App />
     </React.StrictMode>,
   );
