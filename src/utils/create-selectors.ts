38d10c9d7780bd7da9cb54914ad10a912b8792af
kjy5 <kjy5@users.noreply.github.com>
2024-02-21 00:53:21 +0000

    Auto-reformat code

--- src/utils/create-selectors.ts
+++ src/utils/create-selectors.ts
@@ -1,4 +1,4 @@
-import { StoreApi, UseBoundStore } from 'zustand'
+import { StoreApi, UseBoundStore } from "zustand";
 
 type WithSelectors<S> = S extends { getState: () => infer T }
 	? S & { use: { [K in keyof T]: () => T[K] } }
@@ -16,7 +16,7 @@ const createSelectors = <S extends UseBoundStore<StoreApi<object>>>(
 	store.use = {};
 	for (const k of Object.keys(store.getState())) {
 		// biome-ignore lint/suspicious/noExplicitAny: Provided code is for generic use
-		(store.use as any)[k] = () => store((s) => s[k as keyof typeof s]) // skipcq: JS-0323
+		(store.use as any)[k] = () => store((s) => s[k as keyof typeof s]); // skipcq: JS-0323
 	}
 
 	return store;
